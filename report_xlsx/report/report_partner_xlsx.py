62d2624df1121801a47afab380925294a8448d18
Hector <info@hootei.es>
2021-01-26 14:05:55 +0100

    date-range

--- report_xlsx/report/report_partner_xlsx.py
+++ report_xlsx/report/report_partner_xlsx.py
@@ -5,11 +5,12 @@ from odoo import models
 
 
 class PartnerXlsx(models.AbstractModel):
-    _name = 'report.report_xlsx.partner_xlsx'
-    _inherit = 'report.report_xlsx.abstract'
+    _name = "report.report_xlsx.partner_xlsx"
+    _inherit = "report.report_xlsx.abstract"
+    _description = "Partner XLSX Report"
 
     def generate_xlsx_report(self, workbook, data, partners):
-        sheet = workbook.add_worksheet('Report')
-        for i, obj in enumerate(partners):
-            bold = workbook.add_format({'bold': True})
-            sheet.write(i, 0, obj.name, bold)
+        for obj in partners:
+            sheet = workbook.add_worksheet("Report")
+            bold = workbook.add_format({"bold": True})
+            sheet.write(0, 0, obj.name, bold)
